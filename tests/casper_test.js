bb39326bfbe71397a278fd47b74821ee353026c5
Ganesh Iyer <lastlegion@gmail.com>
2017-08-03 17:24:02 -0400

    added tests

---
+++ tests/casper_test.js
@@ -0,0 +1,26 @@
+
+var url = "http://192.168.1.121:8080/select.php";
+
+
+casper.test.begin('Link to flextables works fine', 3, function suite(test) {
+    casper.start(url, function() {
+        test.assertTitle("caMicroscope", "caMicroscope homepage title is the one expected");
+        test.assertExists('a[href="FlexTables/index.php"]', "link to camicroscope is found");
+        /*
+        this.fill('form[action="/search"]', {
+            q: "casperjs"
+        }, true);
+        */
+        this.click('a[href="FlexTables/index.php"]');
+    });
+
+    casper.then(function() {
+
+        test.assertTitle("Flex Tables", "Flextables works fine");
+
+    });
+
+    casper.run(function() {
+        test.done();
+    });
+});
