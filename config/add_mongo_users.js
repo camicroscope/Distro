50292a6bf988f59e2ddcd885216f35d81b246df6
Ryan Birmingham <birm@rbirm.us>
2019-02-20 16:57:41 -0500

    typo fixes

--- config/add_mongo_users.js
+++ config/add_mongo_users.js
@@ -3,9 +3,9 @@ let admins = [{"name":"admin@camicroscope.org", "attrs": ['admin', 'editor']}]
 // edit users
 let editors = [{"name":"editor@camicroscope.org", "attrs": ['admin', 'editor']}]
 // normal users
-let editors = [{"name":"someone@camicroscope.org", "attrs": ['admin', 'editor']}]
+let viewers = [{"name":"someone@camicroscope.org", "attrs": ['admin', 'editor']}]
 // prep for mongo
 
 db.authorization.insertMany(admins)
 db.authorization.insertMany(editors)
-db.authorization.insertMany(editors)
+db.authorization.insertMany(viewers)
