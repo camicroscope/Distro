4220c2e90c07c09eddd0da52b79876581b68c1fb
Ryan Birmingham <birm@rbirm.us>
2019-02-20 20:00:38 -0500

    example users align with ex routers

--- config/add_mongo_users.js
+++ config/add_mongo_users.js
@@ -1,9 +1,9 @@
 // admin users
-let admins = [{"name":"admin@camicroscope.org", "attrs": ['admin', 'editor']}]
+let admins = [{"name":"admin@camicroscope.org", "attrs": ['admin', 'write']}]
 // edit users
-let editors = [{"name":"editor@camicroscope.org", "attrs": ['admin', 'editor']}]
+let editors = [{"name":"editor@camicroscope.org", "attrs": ['write']}]
 // normal users
-let viewers = [{"name":"someone@camicroscope.org", "attrs": ['admin', 'editor']}]
+let viewers = [{"name":"someone@camicroscope.org", "attrs": []}]
 // prep for mongo
 
 db.authorization.insertMany(admins)
