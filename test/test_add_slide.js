f273115bb4aac3f0524f5e50bb8a5f87fd796cab
Ryan Birmingham <birm@rbirm.us>
2019-03-08 15:20:09 -0500

    bump up timeout

--- test/test_add_slide.js
+++ test/test_add_slide.js
@@ -12,6 +12,7 @@ const findurl = "http://localhost:4010/data/Slide/find?slide=TEST"
 describe('Slide Loading Step 1', function () {
 
   it('Posts a Slide', function (done) {
+    this.timeout(5000);
     var slideData = {"name" : "TEST" , "specimen": '', "study": '', "location" : "/images/sample.svs" , "mpp" : 0.499};
     var postProcess = fetch(posturl, {
         "method":"POST",
@@ -34,6 +35,7 @@ describe('Slide Loading Step 1', function () {
 describe('Slide Loading Step 2', function () {
   // can we see it in find
   it('Finds the added slide', function (done) {
+    this.timeout(5000);
     let getProcess = fetch(findurl)
     getProcess.then(x=>x.json()).then(x=>{
       assert.equal(x.length,1, "Slide Shows up in API List")
