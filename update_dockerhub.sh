c37e3b20791f2876764e1497b1ba33170c8bc309
Ryan Birmingham <birm@rbirm.us>
2019-12-04 09:34:12 -0500

    Update update_dockerhub.sh

--- update_dockerhub.sh
+++ update_dockerhub.sh
@@ -6,14 +6,14 @@ docker login
 docker-compose -f quip-pathdb.yml build
 docker-compose -f caMicroscope.yml build
 # tag and push images
-10064  docker tag distro_bindaas:latest camicroscope/bindaas:latest
-10065  docker push camicroscope/bindaas:latest
-10068  docker tag distro_viewer:latest camicroscope/viewer:latest
-10069  docker push camicroscope/viewer:latest
+docker tag distro_bindaas:latest camicroscope/bindaas:latest
+docker push camicroscope/bindaas:latest
+docker tag distro_viewer:latest camicroscope/viewer:latest
+docker push camicroscope/viewer:latest
 docker tag distro_security:latest camicroscope/security:latest
-10071  docker push camicroscope/security:latest
+docker push camicroscope/security:latest
 docker tag distro_auth:latest camicroscope/auth_service:latest
-10074  docker push camicroscope/auth_service:latest
+docker push camicroscope/auth_service:latest
 docker tag distro_imageloader:latest camicroscope/pathdb-imageloader:latest
 docker push camicroscope/pathdb-imageloader:latest
 docker tag distro_heatmaploader:latest camicroscope/pathdb-heatmaploader:latest
