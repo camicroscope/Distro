9ab31488563433de371f68b455cf2255aeea9b66
Ryan Birmingham <birm@rbirm.us>
2021-02-18 10:54:06 -0500

    include volumes

--- k8s/caMicroscope.sh
+++ k8s/caMicroscope.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env sh
-kubectl apply -f caMicroscope/back-deployment.yaml,caMicroscope/loader-deployment.yaml,caMicroscope/idxmongo-deployment.yaml,caMicroscope/mongo-deployment.yaml,caMicroscope/iip-deployment.yaml
+kubectl apply -f caMicroscope/back-claim0-persistentvolumeclaim.yaml,\
+caMicroscope/back-claim1-persistentvolumeclaim.yaml,\
+caMicroscope/back-claim2-persistentvolumeclaim.yaml,\
+caMicroscope/back-deployment.yaml,\
+caMicroscope/back-service.yaml,\
+caMicroscope/idxmongo-claim0-persistentvolumeclaim.yaml,\
+caMicroscope/idxmongo-deployment.yaml,\
+caMicroscope/iip-claim0-persistentvolumeclaim.yaml,\
+caMicroscope/iip-deployment.yaml,\
+caMicroscope/loader-claim0-persistentvolumeclaim.yaml,\
+caMicroscope/loader-deployment.yaml,\
+caMicroscope/mongo-claim0-persistentvolumeclaim.yaml,\
+caMicroscope/mongo-deployment.yaml
--- k8s/pathdb.sh
+++ k8s/pathdb.sh
@@ -1,2 +1,25 @@
 #!/usr/bin/env sh
-kubectl apply -f pathdb/back-deployment.yaml,pathdb/imageloader-deployment.yaml,pathdb/heatmaploader-deployment.yaml,pathdb/mongo-deployment.yaml,pathdb/idxmongo-deployment.yaml,pathdb/pathdb-deployment.yaml,pathdb/iip-deployment.yaml,pathdb/segloader-deployment.yaml
+kubectl apply -f pathdb/back-claim0-persistentvolumeclaim.yaml,\
+pathdb/back-claim1-persistentvolumeclaim.yaml,\
+pathdb/back-deployment.yaml,\
+pathdb/back-service.yaml,\
+pathdb/heatmaploader-claim0-persistentvolumeclaim.yaml,\
+pathdb/heatmaploader-deployment.yaml,\
+pathdb/idxmongo-claim0-persistentvolumeclaim.yaml,\
+pathdb/idxmongo-deployment.yaml,\
+pathdb/iip-claim0-persistentvolumeclaim.yaml,\
+pathdb/iip-deployment.yaml,\
+pathdb/imageloader-claim0-persistentvolumeclaim.yaml,\
+pathdb/imageloader-claim1-persistentvolumeclaim.yaml,\
+pathdb/imageloader-deployment.yaml,\
+pathdb/mongo-claim0-persistentvolumeclaim.yaml,\
+pathdb/mongo-deployment.yaml,\
+pathdb/pathdb-claim0-persistentvolumeclaim.yaml,\
+pathdb/pathdb-claim1-persistentvolumeclaim.yaml,\
+pathdb/pathdb-claim2-persistentvolumeclaim.yaml,\
+pathdb/pathdb-claim3-persistentvolumeclaim.yaml,\
+pathdb/pathdb-claim4-persistentvolumeclaim.yaml,\
+pathdb/pathdb-deployment.yaml,\
+pathdb/pathdb-service.yaml,\
+pathdb/segloader-claim0-persistentvolumeclaim.yaml,\
+pathdb/segloader-deployment.yaml,\
