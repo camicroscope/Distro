50f68f47890219a4e1f588dd40ffea0d32529db2
Ryan Birmingham <birm@rbirm.us>
2022-02-10 10:31:50 -0500

    split configs

---
+++ kubernetes/pathdb_add_config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+kubectl create secret generic camic-jwt-keys --from-file=key=../jwt_keys/key --from-file=key.pub=../jwt_keys/key.pub
+kubectl create configmap pathdb-pre --from-file=pathdb_pre.sh=../config/pathdb_pre.sh
+kubectl create configmap pathdb-config --from-file=pathdb_pre.sh=../config/pathdb
